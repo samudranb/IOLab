ce0010a8e9d6d7f573108b716225805cc72e8f06
Samudra Neelam Bhuyan <samudranb@gmail.com>
2016-02-19 15:23:18 -0800

    incomplete hw4

---
+++ hw4/js/todo.js
@@ -0,0 +1,8 @@
+function addTask() {
+  // add the value of the input field to the todolist
+  var todo = document.getElementById("taskField");
+  console.log("adding new task: " + todo.value);
+  todolist = document.getElementById("todolist");
+
+  
+}
